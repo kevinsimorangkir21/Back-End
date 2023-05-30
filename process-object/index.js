b8c6fcc1aa1cd3b10999001f563b78f229bbf5a7
Kevin Simorangkir <mastercamp436@gmail.com>
2023-05-30 11:38:08 +0700

    Update index.js

--- process-object/index.js
+++ process-object/index.js
@@ -1,5 +1,19 @@
-const message = (name) => {
-  console.log('Hello ${name}');
-}
+// const message = (name) => {
+//   console.log('Hello ${name}');
+// }
 
-message('Java Script');
+// message('Java Script');
+
+const initialMemoryUsage = // TODO 1
+const yourName = // TODO 2
+const environment = // TODO 3
+ 
+for(let i = 0; i <= 10000; i++) {
+// Proses looping ini akan membuat penggunaan memori naik
+}
+ 
+const currentMemoryUsage = // TODO 4
+ 
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`)
+console.log(`Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
